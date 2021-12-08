836b6dc2b92dacb77543ddeb02e1913ca1a87371
z1youRA <374671110@qq.com>
2021-12-08 11:01:16 +0800

    finish repeat str for the number of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    if(times < 0)  
+        return "ERROR";
+    let repeatStr = '';
+    for(let i = 0; i < times; i++) {
+        repeatStr += str;
+    }
+    return repeatStr;
 };
 
 // Do not edit below this line
