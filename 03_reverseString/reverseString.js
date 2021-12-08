1851874aca1c8a3c3e2af20ea80464cbb74f8388
z1youRA <374671110@qq.com>
2021-12-08 11:12:39 +0800

    complete reversestr

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
 const reverseString = function(str) {
-
+    // return str.split("").reverse().join("");
+    const strArr = str.split("");
+    let reversedArr = [];
+    for(let i = 0; i < strArr.length; i++) {
+        reversedArr[i] = strArr[strArr.length - 1 - i];
+    }
+    const reversedStr = reversedArr.join("");
+    return reversedStr;
 };
 
 // Do not edit below this line
