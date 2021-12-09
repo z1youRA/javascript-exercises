b0b87b28e3aaa9cd955028f9b235f3b7d8e676ff
z1youRA <374671110@qq.com>
2021-12-09 17:59:03 +0800

    complete removefrom array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...arg) {
+    // can also use:
+    // arg = [...arguments];
+    // arg.shift();
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < arg.length; j++) {
+            if(arr[i] === arg[j]) {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
