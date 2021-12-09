59693b614e106ea271e057c60c0d9b6e5eb8e7ce
z1youRA <374671110@qq.com>
2021-12-09 18:10:05 +0800

    complete sumall

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(m, n) {
+    if(m < 0 || n < 0)
+        return `ERROR`;
+    if(typeof m !== `number` || typeof n !== `number`)
+        return `ERROR`;
+    if(m > n) {
+        const swap = n;
+        n = m;
+        m = swap;
+    }
+    let sum = 0;
+    for(let i = m; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
