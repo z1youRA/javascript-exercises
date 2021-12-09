2c6707ab0aada42fe7b28d666a31e1d626a08f9e
z1youRA <374671110@qq.com>
2021-12-09 18:23:25 +0800

    complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(fah) {
+  if(typeof fah !== `number`)
+    return `ERROR`;
+  const cel = Math.round(((fah - 32) * 5 / 9) * 10) / 10;
+  return cel;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  if(typeof cel !== `number`)
+    return `ERROR`;
+  const fah = Math.round((cel * 9 / 5 + 32) * 10) / 10;
+  return fah;
 };
 
 // Do not edit below this line
